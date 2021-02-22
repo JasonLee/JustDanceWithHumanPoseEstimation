62842c761892cc3dbb86a2495bcdbb45dce2b863
Jason Lee <43939804+JasonLee@users.noreply.github.com>
2021-02-22 11:15:02 +0000

    added sort/filter with tests (#2)

--- frontend/src/components/SongList.js
+++ frontend/src/components/SongList.js
@@ -10,56 +10,97 @@ import { List, ListItem, Paper, Grow } from '@material-ui/core/';
 class SongList extends Component {
     constructor(props) {
         super(props);
-        this.state  = {
+        this.state = {
             songs: [],
             showPopup: false,
-            popup_content: ""
+            popup_content: "",
+            search: "",
+            drop: "name"
         };
+
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleDropChange = this.handleDropChange.bind(this);
     }
 
     handleClick(song) {
-        this.setState({showPopup: true, popup_content: song});
+        this.setState({ showPopup: true, popup_content: song });
         // console.log(song);
     }
 
+    handleSearchChange(event) {
+        // console.log("search", event.target.value.toLowerCase());
+        this.setState({ search: event.target.value.toLowerCase() });
+    }
+
+    handleDropChange(event) {
+        // console.log("drop", event.target.value.toLowerCase());
+        this.setState({ drop: event.target.value.toLowerCase() });
+    }
+
+    customSort(a, b) {
+        if (a[this.state.drop] == b[this.state.drop]) {
+            return (a.artist > b.artist) ? -1 : 1;
+        }
+
+        return (a[this.state.drop] > b[this.state.drop]) ? 1 : -1;
+    }
     // Runs when component has been added
     componentDidMount() {
         axios.get("/songs")
-        .then(res => {
-            let data = res.data;
-            this.setState({songs: data});
-        }).catch((error) => {
-            console.log(error.toJSON());
-        });
+            .then(res => {
+                let data = res.data;
+                this.setState({ songs: data });
+            }).catch((error) => {
+                console.log(error.toJSON());
+            });
     };
 
+
     removePopup() {
-        this.setState({showPopup: false, popup_content: ""})
+        this.setState({ showPopup: false, popup_content: "" })
     }
 
     // What is actually displayed
     // Note: using index as ID can be bad when reordering
     // Note: key changes result in component remount
     render() {
-        return ( 
+        return (
             <div>
                 <div>
-                    {this.state.showPopup ? 
+                    {this.state.showPopup ?
                         <Grow in={true}>
                             <Paper>
                                 <OutsideAlerter removefunc={() => this.removePopup()}>
-                                    <SongCard key={this.state.popup_content._id}  data={this.state.popup_content}/>
-                                    <Music key={"M"+this.state.popup_content._id}/>
+                                    <SongCard key={this.state.popup_content._id} data={this.state.popup_content} />
+                                    <Music key={"M" + this.state.popup_content._id} />
                                 </OutsideAlerter>
                             </Paper>
                         </Grow>
-                    :   <Paper style={{overflow: 'auto'}} >
-                            <List>
-                                {this.state.songs.map(song=> 
-                                    <ListItem key={song._id}  onClick={() => this.handleClick(song)} ><Song data={song} /></ListItem>
-                                )}
-                            </List>
-                        </Paper>
+                        :
+                        <div>
+                            <div>
+                                <input type="text" value={this.state.search} onChange={this.handleSearchChange} />
+                                <label>
+                                    <select value={this.state.drop} onChange={this.handleDropChange}>
+                                        <option value="name">Song Title</option>
+                                        <option value="artist">Artist</option>
+                                        <option value="difficulty">Difficulty</option>
+                                        <option value="length">Song Length</option>
+                                    </select>
+                                </label>
+                            </div>
+                            <Paper style={{ overflow: 'auto' }} >
+                                <List>
+                                    {this.state.songs
+                                        .filter(song => song.name.toLowerCase().includes(this.state.search) || song.artist.toLowerCase().includes(this.state.search))
+                                        .sort((a, b) => this.customSort(a, b))
+                                        .map(song =>
+                                            <ListItem key={song._id} onClick={() => this.handleClick(song)}> <Song data={song} /> </ListItem>
+                                        )
+                                    }
+                                </List>
+                            </Paper>
+                        </div>
                     }
                 </div>
 
